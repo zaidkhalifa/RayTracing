113933d8189194662e573f7c28c9646aa3c4ffa6
zaidkhalifa <zaid@khalifa.in>
2024-06-24 11:19:19 +0400

    hitbox list and updates

---
+++ hitbox_list.h
@@ -0,0 +1,42 @@
+#ifndef HITBOX_LIST_H
+#define HITBOX_LIST_H
+
+#include "hitbox.h"
+
+#include <memory>
+#include <vector>
+
+using std::make_shared;
+using std::shared_ptr;
+
+class hitbox_list : public hitbox {
+  public:
+    std::vector<shared_ptr<hitbox>> objects;
+
+    hitbox_list() {}
+    hitbox_list(shared_ptr<hitbox> object) { add(object); }
+
+    void clear() { objects.clear(); }
+
+    void add(shared_ptr<hitbox> object) {
+        objects.push_back(object);
+    }
+
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        hit_record temp_rec;
+        bool hit_anything = false;
+        auto closest_so_far = ray_tmax;
+
+        for (const auto& object : objects) {
+            if (object->hit(r, ray_tmin, closest_so_far, temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+};
+
+#endif
---
+++ sphere.h
@@ -0,0 +1,44 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hitbox.h"
+#include "vec3.h"
+
+class sphere : public hitbox {
+  public:
+    sphere(const point3& center, double radius) : center(center), radius(fmax(0,radius)) {}
+
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        vec3 oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius*radius;
+
+        auto discriminant = h*h - a*c;
+        if (discriminant < 0)
+            return false;
+
+        auto sqrtd = sqrt(discriminant);
+
+        auto root = (h - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root) {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root)
+                return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        // rec.normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+
+  private:
+    point3 center;
+    double radius;
+};
+
+#endif
